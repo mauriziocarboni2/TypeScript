203ccd4c5762bb233ea436ef1c87b0972744b7be
mauriziocarboni2 <70142715+mauriziocarboni2@users.noreply.github.com>
2020-10-01 15:24:14 +0300

    Filter should remove a subset of the input type
    Filter should remove a subset of the input type union, not a super set

--- lib/lib.es5.d.ts
+++ lib/lib.es5.d.ts
@@ -1173,7 +1173,7 @@ interface ReadonlyArray<T> {
      * @param predicate A function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array.
      * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
      */
-    filter<S extends T>(predicate: (value: T, index: number, array: readonly T[]) => value is S, thisArg?: any): S[];
+    filter<T extends S>(predicate: (value: T, index: number, array: readonly T[]) => value is S, thisArg?: any): S[];
     /**
      * Returns the elements of an array that meet the condition specified in a callback function.
      * @param predicate A function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array.
